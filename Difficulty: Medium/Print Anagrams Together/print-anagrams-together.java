27ec4f76cc50aaf1a1cec00810d7942a55a01505
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-03-05 19:10:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        HashMap<String,ArrayList<String>> hm=new HashMap<>();
+        for(String word:arr){
+            char[] chars=word.toCharArray();
+            Arrays.sort(chars);
+            String temp=new String(chars);
+            hm.putIfAbsent(temp,new ArrayList<>());
+            hm.get(temp).add(word);
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Ignore the newline after the test case input
+        while (t-- > 0) {
+            String inputLine = sc.nextLine();
+            String[] arr = inputLine.split(" ");
+
+            Solution ob = new Solution();
+            ArrayList<ArrayList<String>> result = ob.anagrams(arr);
+            result.sort(Comparator.comparing(a -> a.get(0)));
+            for (ArrayList<String> group : result) {
+                for (String word : group) {
+                    System.out.print(word + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
