f86d6e4695397caae725b55fa356a0e05166e06a
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-04-05 12:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (88.86%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,30 +1,20 @@
-class Solution {
-    public int subsetXORSum(int[] nums) {
-        if(nums.length==1)
-            return nums[0]; 
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        ArrayList<Integer> a=new ArrayList<>();
-        generateSubSet(0,nums,a,list);
-        int sum=0;
-        for(ArrayList<Integer> i : list){
-            int x=0;
-            for(int j : i)
-            {
-                x^=j;
-            }
-            sum+=x;
+class Solution 
+{
+    private int sum = 0;
+    public void xorsum(int[] nums,int xoro,int ind)
+    {
+        if(ind == nums.length)
+        {
+            sum+=xoro;
+            return ;
         }
-        return sum;
-
+        xorsum(nums,xoro^nums[ind],ind+1);
+        xorsum(nums,xoro,ind+1);
     }
-    public static void generateSubSet(int i,int nums[],ArrayList<Integer> temp,ArrayList<ArrayList<Integer>> list){
-        if(i>=nums.length){
-            list.add(new ArrayList<>(temp));
-            return;
-        }
-        temp.add(nums[i]);
-        generateSubSet(i+1,nums,temp,list);
-        temp.remove(temp.size()-1);
-        generateSubSet(i+1,nums,temp,list);
+    public int subsetXORSum(int[] nums) 
+    {
+        sum=0;
+        xorsum(nums,0,0);
+        return sum;
     }
 }
