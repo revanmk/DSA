1f2b3353108b9f99bcc295b2826fbc9dab6b432b
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-04-11 15:43:18 +0530

    Time: 11 ms (70.77%) | Memory: 58.2 MB (42.56%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,7 +1,19 @@
-
-
-// Brute-Force Approach  
-
+// Optimal Approach  TC:O(n)  SC:O(n)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hs.contains(nums[i]))
+                return true;
+            else
+                hs.add(nums[i]);
+        }
+        return false;
+    }
+}
+// Brute-Force Approach   TC:O(nlogn)  SC:O(1)
+/*
 class Solution{
     public boolean containsDuplicate(int []nums){
         Arrays.sort(nums);
@@ -12,3 +24,4 @@ class Solution{
         return false;
     }
 }
+*/
