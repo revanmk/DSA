0f2e36aad4080a0973b79c870ccb75f285ed051a
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-04-12 10:39:32 +0530

    Time: 83 ms (5.25%) | Memory: 46.1 MB (59.91%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,15 +2,32 @@ import java.util.Arrays;
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int left = 0, right = 0;
         int nums[] = new int[nums1.length + nums2.length];
-        for (int i = 0; i < nums1.length; i++) {
-            nums[i] = nums1[i];
+        int k = 0;
+        while (left < nums1.length && right < nums2.length) {
+            if (nums1[left] <= nums2[right]) {
+                nums[k] = nums1[left];
+                k++;
+                left++;
+            } else {
+                nums[k] = nums2[right];
+                k++;
+                right++;
+            }
         }
-        for (int i = 0; i < nums2.length; i++) {
-            nums[nums1.length + i] = nums2[i];
+        while (left < nums1.length) {
+            nums[k] = nums1[left];
+            k++;
+            left++;
         }
-        Arrays.sort(nums);
-
+        while (right < nums2.length) {
+            nums[k] = nums2[right];
+            k++;
+            right++;
+        }
+        for(int i=0;i<nums.length;i++)
+            System.out.println(nums[i]);
         if (nums.length % 2 == 1) {
             return nums[nums.length / 2];
         } else {
