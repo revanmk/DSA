cbca10a7a10f096ae8dd79fc7957e78df1889113
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-04-25 13:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (94.73%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,16 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode slow=head;
+//         ListNode fast=head;
         
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-}
-            return slow; 
+//         while(fast!=null && fast.next!=null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+// }
+//             return slow; 
         
+//     }
+// }
+class Solution{
+    public ListNode middleNode(ListNode head){
+        int cnt=0;
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            cnt++;
+            temp=temp.next;
+        }
+        if(cnt%2!=0)
+        {
+            for(int i=0;i<cnt/2;i++)
+            {
+                head=head.next;
+            }
+            return head;
+        }
+        else{
+            for(int i=0;i<cnt/2-1;i++)
+            {
+                head=head.next;
+            }
+            return head.next;
+        }
     }
 }
