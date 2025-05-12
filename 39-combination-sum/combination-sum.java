6c8dea6f99d66b9e784a23965ece8a10b051bd95
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-05-12 22:37:18 +0530

    Time: 3 ms (22.83%) | Memory: 44.7 MB (63.79%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,23 @@
 class Solution {
+    private List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        pick(0, target, candidates, ans, new ArrayList<>());
+        Arrays.sort(candidates);
+        generate(new ArrayList<>(),candidates,target,0,0);
         return ans;
     }
-
-    public void pick(int index, int target, int candidates[], List<List<Integer>> ans, List<Integer> curr) {
-        if (index == candidates.length) {
-            if (target == 0)
-                ans.add(new ArrayList<>(curr));
+    public void generate(List<Integer> temp,int[] candidates,int target,int sum,int index){
+        if(sum==target){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        if (candidates[index] <= target) {
-            curr.add(candidates[index]);
-            pick(index, target - candidates[index], candidates, ans, curr);
-            curr.remove(curr.size()-1);
+        if(sum>target)
+            return;
+        for(int i=index;i<candidates.length;i++)
+        {
+            if (i > index && candidates[i] == candidates[i - 1]) continue;
+            temp.add(candidates[i]);
+            generate(temp,candidates,target,sum+candidates[i],i);
+            temp.remove(temp.size()-1);
         }
-        pick(index + 1, target, candidates, ans, curr);
     }
 }
