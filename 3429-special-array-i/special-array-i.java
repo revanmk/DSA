c3a92223dd85e233eea5ef33edc6aba46f60d796
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-05-25 23:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (59.97%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,7 +1,10 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
+        int len=nums.length;
+        if(len==1)
+            return true;
         boolean check=isEven(nums[0]);
-        for(int i=1;i<nums.length;i++)
+        for(int i=1;i<len;i++)
         {
             boolean hahaha=isEven(nums[i]);
             if(hahaha==check)
