11c84c3f87c31b72df45314fb8f0874f07a12778
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-06-17 21:56:40 +0530

    Time: 2 ms (99.49%) | Memory: 42.5 MB (66.38%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,42 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap<>();
-        dfs(root,0,0,map);
-        List<List<Integer>> list = new ArrayList<>();
-        for(TreeMap<Integer,PriorityQueue<Integer>> ys:map.values()){
-            list.add(new ArrayList<>());
-            for(PriorityQueue<Integer> nodes:ys.values()){
-                while(!nodes.isEmpty()){
-                    list.get(list.size()-1).add(nodes.poll());
+        dfs(root, 0, 0);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (TreeMap<Integer, PriorityQueue<Integer>> vertical : map.values()) {
+            List<Integer> col = new ArrayList<>();
+            for (PriorityQueue<Integer> nodes : vertical.values()) {
+                while (!nodes.isEmpty()) {
+                    col.add(nodes.poll());
                 }
             }
+            result.add(col);
         }
-        return list;
+
+        return result;
     }
-    public void dfs(TreeNode root,int x,int y,TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map){
-        if(root==null)
-            return;
-        if(!map.containsKey(x))
-            map.put(x,new TreeMap<>());
-        if(!map.get(x).containsKey(y))
-            map.get(x).put(y,new PriorityQueue<>());
-        map.get(x).get(y).offer(root.val);
-        dfs(root.left,x-1,y+1,map);
-        dfs(root.right,x+1,y+1,map);
+
+    public void dfs(TreeNode root, int hd,int vd) {
+        if (root == null) return;
+
+       if(!map.containsKey(hd)){
+        map.put(hd,new TreeMap<>());
+       }
+       if(!map.get(hd).containsKey(vd)){
+        map.get(hd).put(vd, new PriorityQueue<>());
+       }
+       map.get(hd).get(vd).add(root.val);
+        dfs(root.left, hd - 1,vd+1);
+        dfs(root.right, hd + 1,vd+1);
     }
 }
