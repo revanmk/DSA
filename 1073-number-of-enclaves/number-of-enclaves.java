a118b7a8c5db771eeebf973020661d2cdea3ca2c
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-06-24 16:36:20 +0530

    Time: 9 ms (60.10%) | Memory: 62.8 MB (5.02%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int vis[][] = new int[rows][cols];
+        for(int i=0;i<cols;i++)
+        {
+            if(grid[0][i]==1 && vis[0][i]==0){
+                dfs(grid,vis,0,i);
+            }
+            if(grid[rows-1][i]==1 && vis[rows-1][i]==0){
+                dfs(grid,vis,rows-1,i);
+            }
+        }
+        for(int i=0;i<rows;i++)
+        {
+            if(grid[i][0]==1 && vis[i][0]==0){
+                dfs(grid,vis,i,0);
+            }
+            if(grid[i][cols-1]==1 && vis[i][cols-1]==0){
+                dfs(grid,vis,i,cols-1);
+            }
+        }
+        int count=0;
+
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(grid[i][j]==1 && vis[i][j]==0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(int[][] grid, int[][] vis, int row, int col){
+
+        int rows=grid.length;
+        int cols=grid[0].length;
+        vis[row][col]=1;
+        int del_rows[]={-1,0,+1,0};
+        int del_cols[]={0,+1,0,-1};
+
+        for(int i=0;i<4;i++)
+        {
+            int nrow=row+del_rows[i];
+            int ncol=col+del_cols[i];
+
+            if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                dfs(grid,vis,nrow,ncol);
+        }
+    }
+}
