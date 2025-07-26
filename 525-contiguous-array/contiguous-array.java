6bfd00df34822d1412d8d5beab683dd6dbc26e8d
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-07-26 14:28:26 +0530

    Time: 23 ms (81.40%) | Memory: 55.7 MB (17.45%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -4,21 +4,17 @@ class Solution {
         int len=nums.length;
         int ans=0;
         int max=0;
+        map.put(0,-1);
         for(int i=0;i<len;i++){
             if(nums[i]==0){
                 ans-=1;
             }else{
                 ans+=1;
             }
-            if(ans == 0){
-                max = i+1;
-            }
-            else{
                 if(map.containsKey(ans))
                     max=Math.max(max,i-map.get(ans));
                 else
                     map.put(ans,i);
-            }
             
         }
         return max;
