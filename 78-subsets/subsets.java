0da5a2186f0c2c50db40800536b32652d0f244d8
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-07-31 10:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (10.02%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,17 @@
 class Solution {
-    private List<List<Integer>> ans=new ArrayList<>();
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        raparapa(new ArrayList<>(),0,n,nums);
+        generateSubsets(0,nums,new ArrayList<>());
         return ans;
     }
-    public void raparapa(List<Integer> curr,int index,int n,int [] nums){
-        if(index>=n){
+    public void generateSubsets(int index,int[] nums,List<Integer> curr){
+        if(index>=nums.length){
             ans.add(new ArrayList<>(curr));
             return;
         }
         curr.add(nums[index]);
-        raparapa(curr,index+1,n,nums);
+        generateSubsets(index+1,nums,curr);
         curr.remove(curr.size()-1);
-        raparapa(curr,index+1,n,nums);
-
+        generateSubsets(index+1,nums,curr);
     }
 }
