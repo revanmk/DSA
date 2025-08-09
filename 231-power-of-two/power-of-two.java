e47634e46d623780a64979944b2cb29180506eec
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-08-09 18:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (17.04%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
-        }
-        return bitCount(n)==1;
-    }
-    public static int bitCount(int n){
-        int count=0;
-        for(int i=0;i<32;i++){
-            if(((n>>i)&1)==1){
-                count++;
-            }
-        }
-        return count;
+        return (n>0 && (n&(n-1))==0);
     }
 }
