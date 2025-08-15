0dd32403da36f8bd5ab8df7cde10ad7c4afe9508
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-08-15 20:12:36 +0530

    Time: 141 ms (15.75%) | Memory: 44.4 MB (28.08%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<Integer> specials = new ArrayList<>();
+
+    public int nonSpecialCount(int l, int r) {
+        generateSpecialNumbers();
+        int ans = r-l+1;
+        for(int a : specials){
+            if(a>=l && a<=r){
+                ans--;
+            }
+        }
+        return ans;
+    }
+
+    public void generateSpecialNumbers() {
+        int limit = (int)Math.sqrt(1e9);
+        boolean prime[] = new boolean[limit + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i*i <= limit; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= limit; j+=i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        for (int i = 0; i < limit + 1; i++) {
+            if (prime[i]) {
+                specials.add(i * i);
+            }
+        }
+    }
+}
