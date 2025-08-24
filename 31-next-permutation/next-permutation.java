4b7fb14404b5c106edf178f707514304c457313d
Mk.Revan <153989080+revanmk@users.noreply.github.com>
2025-08-24 09:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (5.94%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,36 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int len = nums.length;
-        int ind=-1;
-        for(int i=len-2;i>=0;i--){
+        int ind =-1;
+        for(int i = len-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                ind=i;
+                ind = i;
                 break;
             }
         }
-        if(ind==-1){
-            reverse(0,len-1,nums);
+        if(ind ==-1){
+            nums = reverse(nums,0,len-1);
             return;
         }
         for(int i=len-1;i>ind;i--){
             if(nums[i]>nums[ind]){
                 int temp = nums[i];
-                nums[i]=nums[ind];
-                nums[ind]=temp;
+                nums[i] = nums[ind];
+                nums[ind] = temp;
                 break;
             }
         }
-        nums=reverse(ind+1,len-1,nums);
+        nums = reverse(nums,ind+1,len-1);
     }
-    public static int[] reverse(int start, int end, int[] nums){
-        while(start<end){
-            int temp =  nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
-            start++;
-            end--;
+    public static int[] reverse(int[] nums, int start, int end){
+        for(int i=start;i<=(start+end)/2;i++){
+            int temp = nums[i];
+            nums[i] = nums[end-(i-start)];
+            nums[end-(i-start)] = temp; 
         }
         return nums;
     }
-
 }
